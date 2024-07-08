f9ca3bf34a5142ac44f022b9a883b21f74ff44b0
Monesh-003 <moneshn003@gmail.com>
2024-07-08 11:47:43 +0530

    web page

---
+++ src/App.js
@@ -0,0 +1,239 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Niice
+        </a> */}
+        <div className='full'>
+          <div className='top'>
+          <table>
+            <td><h4>introducing Niice Docs: online guideliness that envolve with your Brand. | Find out more </h4></td>
+          </table>
+          </div>
+        <div className='main'>
+          <table>
+          <tr>
+          <td style={{filter:" contrast(3)"}}><img src='https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQMz94Zi4C7892wFkwnyAdeE_sZp7E5SdRQGjlGiaSKncHCfPFB' width='120px' height='90px'/></td>
+          <td style={{padding:"10px"}}> Use Cases</td>
+          <td  style={{padding:"10px",}}><a href='coustmer.html'>Customers</a></td>
+          <td  style={{padding:"10px"}}><a href='pricing.html'>Pricing</a></td>
+          </tr>
+          </table>
+        </div>
+        <div className='two'>
+          <table>
+            <tr>
+              <td  style={{padding:"10px"}}>Example</td>
+              <td  style={{padding:"10px"}}><a href='template.html'>Templates</a></td>
+              <td  style={{padding:"10px"}}> <a href='sigin.html'>Sign</a></td>
+              <td><button style={{borderRadius:"20px", height:"30px", backgroundColor:"black"}}><a href='hub.html'>Create a Brand Hub</a></button></td>
+            </tr>
+            </table>       
+        </div>
+        <div className='smalline'>
+          <h2>The Brand Hub for Remote Creative Teams</h2> 
+        </div>
+        <div className='concent'> 
+          <h1> Show what your </h1>
+          <h1> brand is made of</h1>
+        </div>
+        <div className='longline'>
+          <p>Bring your guidelines, libraries, projects and people together in</p>
+          <p>a brand hub that untangles access and gives creative assets</p>  
+          <p> the attention they deserve.</p>  
+        </div>
+        <div className='smallbutton'>
+         <button  style={{borderRadius:"20px", height:"30px",backgroundColor:"red",color:"white",height:"40px"}}> Start Your brand hub</button>
+        </div>
+         <div className='siximg'>
+          <img src='https://niice.dreamhosters.com/wp-content/uploads/2022/06/logos.svg' width='800px' height='300px'></img>
+         </div>
+           <div className='aimage'>
+           <img src='https://niice.dreamhosters.com/wp-content/uploads/2022/07/Slice-9-2.svg'></img>
+           </div>
+           <div className='bimage'> 
+             <img src='https://niice.dreamhosters.com/wp-content/uploads/2022/07/Frame-32-2.svg' width='800px' height='1300px'></img>
+           </div>
+           <div className='centervideo'>
+            {/* <a href=''><iframe height="315px" width="700px" video src='home.html' ></iframe></a> */}
+           </div>
+           <div className='nextfull'>
+           <div className='fimage'>
+                 <img src='https://niice.dreamhosters.com/wp-content/uploads/2022/07/Frame-331.svg' width='800px' height='1300px'></img>
+           </div>
+           <div className='smallbox'>
+                <div className='sb'>
+                <p>Niice enabled us to develop new workflows and</p>
+            <p>get non-creatives more involved. For us, it has</p>
+            <p>evolved from a creative review tool to a full brand</p>
+            <p style={{textAlign:"center"}}>hub for all of our assets</p>
+                </div>
+           </div>
+           <div className='triangleimg'>
+            <img src='https://study.com/cimages/multimages/16/problem_16799387787362449882.png'width='1530px' height='400px' ></img>
+           </div>
+           <div className='white'>
+            <img src='data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBwgHBgkIBwgKCgkLDRYPDQwMDRsUFRAWIB0iIiAdHx8kKDQsJCYxJx8fLT0tMTU3Ojo6Iys/RD84QzQ5OjcBCgoKDQwNGg8PGjclHyU3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3N//AABEIAJQBDgMBIgACEQEDEQH/xAAXAAEBAQEAAAAAAAAAAAAAAAAAAQIH/8QAGRABAQEAAwAAAAAAAAAAAAAAAAERAiEx/8QAFAEBAAAAAAAAAAAAAAAAAAAAAP/EABQRAQAAAAAAAAAAAAAAAAAAAAD/2gAMAwEAAhEDEQA/AOwxpIoAKCNAAGLgBi4oM4uLi4DOLi4AhjQDOGNAM4Y0lBMMUwEqNYWAzUxrCwGUaqAhVqAgpQZ5es1tLOwAUBUUBcIoGKKBgKACggoCKAIKAkUASigIlaSgiVpKCFVKCVK1UoMlWoCCgIoALBQFFAUABQAAAAAAAAAAAAAABFARKoCVKpQZqNVkEFqYBA1YCqkUFVFAUAAAAAAAAAAAItQAAAAAAARQERaAzUrbNBEoAzI0jUBYsRQUAFAAAAAAAAAAAAi1AAAAAAAAEAABKlWpQKyoDMajMagKqRQUAFAAAAAAAAAAAAAAAAAAAAABKFAShQGUarII0zGoCqigoAKIoAAAAAAAAAAAAAAAAAAAAJQASpVqAVlaAzFjLUBpWYoKqKAqAKIoAAAAAAAAAAAAAAAACAAJQCoIBUEBNWMNaDUWMxYDSsxdBoRQAAAAUQBRAFEAUQBRAFEAAABACpQoImiAFNibAZUAVUAaigCqAAAKAAAAAAAAAAAAACUABKoDNQAKlQBKAD//2Q=='width='1530px' height='900px'></img>
+            <div className='smalllitter'>
+              <p><h2 style={{textAlign:"center"}}>Viva visibility.</h2></p>
+              <p style={{fontSize:"30px",color:"blue",paddingTop:"-0px", paddingRight:"-500px"}}>Beautifully present assets on canvases where you control the</p>
+              <p style={{fontSize:"30px", color:"blue"}}>Composition ,and say"so long!" to miserable folder system.</p>
+            </div>
+            <div className='smone'>
+             <img src='https://niice.dreamhosters.com/wp-content/uploads/2022/07/Frame-31-2-1536x1157.jpg'width='1500px' height='1100px'></img>
+            </div>
+            <div className='smtwo'>
+              <img src='https://niice.dreamhosters.com/wp-content/uploads/2022/07/Group-823-1.svg'></img>
+            </div>
+           </div>
+           <div className='contone'>
+               <p><h2>Put your brand within</h2></p>
+               <p style={{textAlign:"center"}}><h2>their grasp</h2></p>
+               <p style={{textAlign:"center", fontSize:"20px"}}>More than just a brand repository, Niice helps you transform</p>
+               <p style={{textAlign:"center", fontSize:"20px"}}>how well teams and partners grasp, find, and use your</p>
+               <p style={{textAlign:"center", fontSize:"20px"}}>brand assets.</p>
+           </div>
+           <div className='conttwo'>
+           <table>
+            <tr>
+              <td>
+             <img src='https://niice.dreamhosters.com/wp-content/uploads/2022/07/Slice-8-1.jpg'width='500px' height='350px'></img>
+            </td>
+           
+            <tr className='subtwo'>
+              <td>
+              <h1>Protect brand integrity with
+              guide that in
+              understanding</h1>
+              <p><a href=''>Make brand guidelines more </a></p>
+              </td>
+              </tr>
+              </tr>
+           </table>
+           </div>
+
+            <div className='contree'>
+               <table>
+                <tr>
+                  <td>
+                  <h1>Find files faster<br></br>
+                      with beautifully<br></br>
+                      clear collections</h1>
+                 <p><a href=''>Explore our asset libraries </a></p>
+                  </td>
+                  <tr className='subtree'>
+                    <img src='https://niice.dreamhosters.com/wp-content/uploads/2022/07/Slice-7-1-768x501.jpg'width='500px' height='350px'></img>
+                  </tr>
+                </tr>
+               </table>
+            </div>
+
+            <div className='contfour'>
+           <table>
+            <tr>
+              <td>
+             <img src='https://niice.dreamhosters.com/wp-content/uploads/2022/07/Frame-590-6-1-1536x1520.jpg'width='500px' height='450px'></img>
+            </td>
+           
+            <tr className='subfour'>
+              <td>
+              <h1>Handover a brand<br></br>
+                 story, not just <br></br> 
+                 deliverables</h1>
+              <p><a href=''> Bring your creative projects together</a></p>
+              </td>
+              </tr>
+              </tr>
+           </table>
+           </div>
+           <div className='lineimgone'>
+              <img src='https://niice.dreamhosters.com/wp-content/uploads/2022/05/Slice-3.svg'></img>
+           </div>
+           <div className='bgtwo'>
+              <img src='https://wallpapercave.com/wp/wp6431925.jpg'width='1450' height='500px'></img>
+           </div>
+           <div className='lastone'>
+           <p><h1>Made for mountains of content</h1></p>
+                <table>
+               <tr>
+                <td>
+                <p style={{fontSize:"15px"}}>Brands like NORDSTROM <br></br>
+                   are adding over</p>
+                 <p style={{fontSize:"20px"}}><h1>10,000+</h1></p>
+                 <p style={{fontSize:"15px"}}>assets every month</p>
+                   </td>
+               
+               <tr>
+                <td className='lastsubone'>
+               <p style={{fontSize:'15px'}}><h2>"Concepting, developing and sharing our brand vision was<br></br>
+                 time consuming and siloed. Niice made it intuitive, fast and<br></br>
+                  collaborative, saving us over $1 Million per year."</h2></p>
+                 <p style={{fontSize:'15px',}}><img src='https://niice.dreamhosters.com/wp-content/uploads/2022/07/amanda.jpg' width='50px' height='50px'></img>Amanda Jack, Nordstrom</p>
+                  </td>
+                  </tr>
+                  </tr>
+                  </table>
+           </div>
+           <div className='arrowimg'>
+              <img src='https://niice.dreamhosters.com/wp-content/uploads/2022/05/Slice-4.svg' height="370" width="1750"></img>
+           </div>
+           <div className='pinkbg'>
+               <img src='https://upload.wikimedia.org/wikipedia/commons/2/2b/ColorAmaranth.jpg?20081125090419' width='1580px' height='310px'></img>
+           </div>
+           <div className='lastline'>
+           <p><h1>Your brand, like you’ve <br></br>
+           never seen it before <br></br> <button paddingTop='50%'>Geat Start For Free</button></h1>
+          </p>
+           </div>
+           <div className='lastlineimg'>
+              <img src='https://niice.dreamhosters.com/wp-content/uploads/2022/07/Group-822.svg' width="1600px"></img>
+           </div>
+            <div className='last'>
+             <table>
+              <tr>
+                <td style={{paddingRight:"20px",filter:" contrast(3)"}}>
+                  <img src='https://seandalton.com.au/wp-content/uploads/2014/06/Niice.jpg ' width='150px' height='100px'></img>
+                </td>
+                <th style={{paddingRight:"50px"}}>
+                <tr>Features</tr>
+                <tr>Pricing</tr>
+                <tr>Examples</tr>
+                <tr>Customers</tr>
+                <tr>Partners</tr>
+                </th>
+                <th>
+                <tr>Integrations</tr>
+                <tr>Help</tr>
+                <tr>Blog</tr>
+                <tr>Inspiration</tr>
+                <tr>Privacy</tr>
+                </th>
+                </tr>
+                
+             </table>
+            </div>
+            </div>
+
+      </div>
+      </header>
+      
+    </div>
+    
+  );
+
+}
+
+export default App;
